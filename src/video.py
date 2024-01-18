b79bd305c1ba549834d79585e6e658ae60cf9f08
koipish <koipish@gmail.com>
2024-01-18 20:18:44 +0300

    added class PLVideo

--- src/video.py
+++ src/video.py
@@ -1,8 +1,8 @@
-import json
 from googleapiclient.discovery import build
 import os
 
 class Video:
+
     API_KEY: str = os.getenv('API_YouTube')
 
     def __init__(self, video_id):
@@ -11,14 +11,22 @@ class Video:
                                            id=self.video_id
                                            ).execute()
         self.video_title: str = self.video_response['items'][0]['snippet']['title']
+        self.url = f"https://www.youtube.com/watch?v={self.video_response['items'][0]['id']}'"
         self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
         self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
-        self.comment_count: int = self.video_response['items'][0]['statistics']['commentCount']
+
+    def __str__(self):
+        return f'{self.video_title}'
 
     @classmethod
     def get_service(cls):
         return build('youtube', 'v3', developerKey=cls.API_KEY)
 
+class PLVideo(Video):
+
+    def __init__(self, video_id, playlist_id):
+        super().__init__(video_id)
+        self.playlist_id = playlist_id
 
 
 
